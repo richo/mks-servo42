cab429f01eac1170f81d83852d886c7bdd1ab651
Richo Healey <richo@psych0tik.net>
2024-11-27 00:22:52 -0800

    Start sketching out a real API

--- examples/desktop.rs
+++ examples/desktop.rs
@@ -4,10 +4,45 @@ use serial;
 use serial::{
     SerialPort,
 };
+use std::fmt;
 use std::io::{
-    Error,
+    Error as IoError,
     Write,
 };
+
+enum Error {
+    Servo(mks_servo42::Error),
+    Io(IoError),
+    Serial(serial::Error),
+}
+
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::Servo(servo) => write!(f, "Servo {{ {:?} }}", servo.as_str()),
+            Error::Io(io) => write!(f, "Io {{ {:?} }}", io),
+            Error::Serial(serial) => write!(f, "Serial {{ {:?} }}", serial),
+        }
+    }
+}
+
+impl From<mks_servo42::Error> for Error {
+    fn from(other: mks_servo42::Error) -> Error {
+        Error::Servo(other)
+    }
+}
+
+impl From<IoError> for Error {
+    fn from(other: IoError) -> Error {
+        Error::Io(other)
+    }
+}
+
+impl From<serial::Error> for Error {
+    fn from(other: serial::Error) -> Error {
+        Error::Serial(other)
+    }
+}
 
 fn main() -> Result<(), Error> {
     let mut driver = mks_servo42::Driver::default();
@@ -18,9 +53,7 @@ fn main() -> Result<(), Error> {
         Ok(())
     })?;
 
-    s.write_all(driver.rotate())?;
-
-
+    s.write_all(driver.rotate(mks_servo42::direction::Direction::Forward, 100)?)?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,42 @@
 #![no_std]
 
 mod response;
+pub mod direction;
+mod errors;
 
-#[derive(Default)]
+pub use errors::Error;
+
+
+
 pub struct Driver {
-    // Address: we'll default to e0
+    address: u8,
     bytes: [u8; 8], // This is our buffer that we're going to load out with commands in it
 }
 
+type Result<T> = core::result::Result<T, crate::Error>;
+
+impl Default for Driver {
+    fn default() -> Self {
+        Driver {
+            address: 0xe0,
+            bytes: Default::default(),
+        }
+    }
+}
+
 impl Driver {
-    pub fn rotate<'a>(&'a mut self) -> &'a mut [u8] {
+    pub fn rotate<'a>(&'a mut self, direction: direction::Direction, speed: u8) -> Result<&'a mut [u8]> {
+        if speed > 0x80 {
+            return Err(Error::InvalidValue);
+        }
+
         // For now we'll just build it up by hand because we're being silly
-        let cmd = [0xe0, 0xf6, 0x01];
+        let cmd = [self.address, 0xf6, speed & direction as u8];
         let len = cmd.len();
         self.bytes[..len].clone_from_slice(&cmd);
         self.bytes[len] = checksum(&cmd);
 
-        &mut self.bytes[..len+1]
+        Ok(&mut self.bytes[..len+1])
     }
 }
 
